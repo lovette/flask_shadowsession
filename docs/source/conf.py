34f6a1607bb58823514019f284823e33b113b62f
Lance Lovette <lance.lovette@gmail.com>
2024-10-19 16:04:25 -0400

    2to3: Remove Unicode string literal prefix.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,8 +44,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Flask-ShadowSession'
-copyright = u'2014, Lance Lovette'
+project = 'Flask-ShadowSession'
+copyright = '2014, Lance Lovette'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -197,8 +197,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'Flask-ShadowSession.tex', u'Flask-ShadowSession Documentation',
-   u'Lance Lovette', 'manual'),
+  ('index', 'Flask-ShadowSession.tex', 'Flask-ShadowSession Documentation',
+   'Lance Lovette', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -227,8 +227,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'flaskshadowsession', u'Flask-ShadowSession Documentation',
-     [u'Lance Lovette'], 1)
+    ('index', 'flaskshadowsession', 'Flask-ShadowSession Documentation',
+     ['Lance Lovette'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -241,8 +241,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Flask-ShadowSession', u'Flask-ShadowSession Documentation',
-   u'Lance Lovette', 'Flask-ShadowSession', 'One line description of project.',
+  ('index', 'Flask-ShadowSession', 'Flask-ShadowSession Documentation',
+   'Lance Lovette', 'Flask-ShadowSession', 'One line description of project.',
    'Miscellaneous'),
 ]
 
